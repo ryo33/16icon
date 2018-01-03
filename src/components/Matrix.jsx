8b854c1453d18dcff2743ec4b0bc912cdd4b2fa8
Hashiguchi Ryo <ryo12redstone@gmail.com>
2018-01-04 03:31:00 +0900

    Use viewBox

--- src/components/Matrix.jsx
+++ src/components/Matrix.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const mapStateToProps = ({matrix, width, height}) => {
-  return {matrix, width, height}
+const mapStateToProps = ({matrix}, props) => {
+  return {matrix, ...props}
 }
 
-const size = 64
-
-const Matrix = ({ matrix, width, height }) => {
+const Matrix = ({ matrix, width='200px', height='200px' }) => {
   const svgMatrix = matrix.reduce((acc, line, y) => {
     acc.push(line.map(({ h, s, l }, x) => (
       <rect
         key={`${x}-${y}`}
-        x={x*size} y={y*size}
-        width={`${size}px`}
-        height={`${size}px`}
+        x={x} y={y}
+        width={1}
+        height={1}
         fill={`hsl(${h}, ${Math.round(100*s)}%, ${Math.round(100*l)}%)`}
       />
     )))
     return acc
   }, [])
-  return <svg style={{width: `${width*size}px`, height:`${height*size}px`}}>
+  const viewBox = `0 0 ${matrix[0].length} ${matrix.length}`
+  return <svg style={{width, height}} viewBox={viewBox}>
     {svgMatrix}
   </svg>
 }
