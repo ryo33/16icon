81fe4a36f12e0bafb7d2da0c8ce774256a933dc8
Hashiguchi Ryo <ryo12redstone@gmail.com>
2018-01-05 00:00:41 +0900

    Sync parameters with url queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,84 @@ import React, { Fragment } from 'react'
 import Link from 'gatsby-link'
 import Store from 'repatch'
 import { Provider } from 'react-redux'
+import ReduxQuerySync from 'redux-query-sync'
+import isBrowser from 'is-browser'
+import base64url from 'base64-url'
 
 import RefreshButton from '../components/RefreshButton'
 import NumericInputs from '../components/NumericInputs'
 import Icons from '../components/Icons'
 import ColorPicker from '../components/ColorPicker'
 import Sliders from '../components/Sliders'
-import { initialState } from '../reducers'
+import {
+  refresh,
+  initialState,
+  updateLength,
+  updateWidth,
+  updateHeight,
+  changeColor,
+  updateStandardDeviation
+} from '../reducers'
 
 const store = new Store(initialState)
+const defaultState = store.getState()
+if (isBrowser) {
+  const stringToValue = string => {
+    if (string) return JSON.parse(base64url.decode(string))
+  }
+  const valueToString = value => {
+    return base64url.encode(JSON.stringify(value))
+  }
+  ReduxQuerySync({
+    store,
+    params: {
+      length: {
+        selector: state => state.length,
+        action: updateLength,
+        defaultValue: defaultState.length,
+        stringToValue, valueToString
+      },
+      width: {
+        selector: state => state.width,
+        action: updateWidth,
+        defaultValue: defaultState.width,
+        stringToValue, valueToString
+      },
+      height: {
+        selector: state => state.height,
+        action: updateHeight,
+        defaultValue: defaultState.height,
+        stringToValue, valueToString
+      },
+      base: {
+        selector: state => state.base,
+        action: changeColor,
+        defaultValue: defaultState.base,
+        stringToValue, valueToString
+      },
+      hue: {
+        selector: state => state.standardDeviations.h,
+        action: value => updateStandardDeviation('h', value),
+        defaultValue: defaultState.standardDeviations.h,
+        stringToValue, valueToString
+      },
+      satuation: {
+        selector: state => state.standardDeviations.s,
+        action: value => updateStandardDeviation('s', value),
+        defaultValue: defaultState.standardDeviations.s,
+        stringToValue, valueToString
+      },
+      lightness: {
+        selector: state => state.standardDeviations.l,
+        action: value => updateStandardDeviation('l', value),
+        defaultValue: defaultState.standardDeviations.l,
+        stringToValue, valueToString
+      },
+    },
+    initialTruth: 'location',
+  })
+  store.dispatch(refresh())
+}
 
 // TODO Use `<></>` instead of `<div></div>`.
 const App = () => (
